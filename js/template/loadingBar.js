1202cc1295a3bae548e4500859473ad507ec3ba2
jho2301 <jho2301@gmail.com>
2020-09-10 20:29:17 +0900

    전환오1

---
+++ js/template/loadingBar.js
@@ -0,0 +1,12 @@
+// 로딩 바
+<li>
+  <div class="view">
+    <label class="label">
+      <div class="animated-background">
+        <div class="skel-mask-container">
+          <div class="skel-mask"></div>
+        </div>
+      </div>
+    </label>
+  </div>
+</li>;
---
+++ js/template/userSelect.js
@@ -0,0 +1,5 @@
+<button class="ripple active">eastjun</button>
+<button class="ripple">westjun</button>
+<button class="ripple">southjun</button>
+<button class="ripple">northjun</button>
+<button class="ripple">hojun</button>
